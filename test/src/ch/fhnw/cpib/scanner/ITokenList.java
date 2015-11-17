e7fae442bd61122b8af62ab06f4159e9aeb09c02
jakobruder <jakob.ruder@ubs.com>
2015-11-17 18:08:32 +0100

    Fixed error in ITokenList

--- test/src/ch/fhnw/cpib/scanner/ITokenList.java
+++ test/src/ch/fhnw/cpib/scanner/ITokenList.java
@@ -1,11 +1,11 @@
 package ch.fhnw.cpib.scanner;
 
 public interface ITokenList {
-	public void add(IToken.Base token);
+	public void add(IToken token);
 
 	public void reset();
 
-	public IToken.Base nextToken();
+	public IToken nextToken();
 
 	public String toString();
 }
