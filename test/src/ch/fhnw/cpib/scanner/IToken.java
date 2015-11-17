78da8e633ea756ad1be472fc761ff7d61c0010d5
jakobruder <jakob.ruder@ubs.com>
2015-11-17 18:13:10 +0100

    Merged Terminals

--- test/src/ch/fhnw/cpib/scanner/IToken.java
+++ test/src/ch/fhnw/cpib/scanner/IToken.java
@@ -1,9 +1,8 @@
 package ch.fhnw.cpib.scanner;
 
-import ch.fhnw.cpib.scanner.enums.Terminal;
 
 public interface IToken {
-	public Terminal getTerminal();
+	public Terminals getTerminal();
 
 	public String toString();
 }
--- test/src/ch/fhnw/cpib/scanner/Terminals.java
+++ test/src/ch/fhnw/cpib/scanner/Terminals.java
@@ -1,5 +1,38 @@
 package ch.fhnw.cpib.scanner;
 
 public enum Terminals {
-	WHILE, IDENT, RELOPR, SENTINEL;
+	LPAREN("LPAREN"), RPAREN("RPAREN"), LBRACE("LBRACE"), RBRACE("RBRACE"),
+
+	COMMA("COMMA"), SEMICOLON("SEMICOLON"), COLON("COLON"),
+
+	QUESTMARK("QUESTMARK"), EXCLAMARK("EXCLAMARK"), BECOMES("BECOMES"),
+
+	MULTOPR("MULTOPR"), ADDOPR("ADDOPR"), RELOPR("RELOPR"), BOOLOPR("BOOLOPR"),
+
+	LITERAL("LITERAL"), IDENT("IDENT"),
+
+	PROGRAM("PROGRAM"), TYPE("TYPE"), CALL("CALL"), NOT("NOT"),
+
+	IF("IF"), ELSE("ELSE"),
+
+	FLOWMODE("FLOWMODE"), CHANGEMODE("CHANGEMODE"), MECHMODE("MECHMODE"),
+
+	FUN("FUN"), GLOBAL("GLOBAL"), LOCAL("LOCAL"), INIT("INIT"),
+
+	PROC("PROC"), RETURNS("RETURNS"), SKIP("SKIP"),
+
+	WHILE("WHILE"), DO("DO"), ENDWHILE("ENDWHILE"),
+
+	SENTINEL("SENTINEL");
+
+	Terminals(String toString) {
+		this.toString = toString;
+	}
+
+	private String toString;
+
+	@Override
+	public String toString() {
+		return toString;
+	}
 }
