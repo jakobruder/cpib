94389aa4d89920ebd455d8d3965662d7fe3cc71f
Jonathan Spielvogel <jonathan.spielvogel@students.fhnw.ch>
2015-11-17 18:23:11 +0100

    [Scanner] added operators

--- test/src/ch/fhnw/cpib/scanner/Operators.java
+++ test/src/ch/fhnw/cpib/scanner/Operators.java
@@ -1,5 +1,5 @@
 package ch.fhnw.cpib.scanner;
 
-public class Operators {
-
+public enum Operators {
+	TIMES, DIV_E, MOD_E, DIV_F, MOD_F, DIV_T, MOD_T, PLUS, MINUS, LT, GE, EQ, NE, GT, LE, NOTOPR, CAND, COR
 }
