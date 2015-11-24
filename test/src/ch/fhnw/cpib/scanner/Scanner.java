7095e0002981b0ea912afed8e4ac2aa80e26e165
jakobruder <jakob.ruder@ubs.com>
2015-11-24 19:37:20 +0100

    Fixed end of the TokenList

--- test/src/ch/fhnw/cpib/scanner/Scanner.java
+++ test/src/ch/fhnw/cpib/scanner/Scanner.java
@@ -2,33 +2,33 @@ package ch.fhnw.cpib.scanner;
 
 import java.util.LinkedList;
 
+import ch.fhnw.cpib.scanner.enums.Terminals;
 import ch.fhnw.cpib.scanner.exceptions.ScannerException;
 import ch.fhnw.cpib.scanner.interfaces.IScannerState;
 import ch.fhnw.cpib.scanner.interfaces.IToken;
 import ch.fhnw.cpib.scanner.states.DefaultState;
+import ch.fhnw.cpib.scanner.symbols.Base;
 
 public class Scanner {
 
 	private IScannerState state = new DefaultState();
 
-	private static final String[] ALL_SYMBOLS = new String[] { "(", ")", ",",
-			";", ":", ":=", "*", "+", "-", "=", "/=", "<", ">", "<=", ">=",
-			"&?", "|?" };
-
 	public LinkedList<IToken> scan(CharSequence program) {
 		LinkedList<IToken> tokenList = new LinkedList<>();
-		StringBuffer currentString = new StringBuffer();
+		try {
+			for (int i = 0; i < program.length(); i++) {
+				String currentChar = String.valueOf(program.charAt(i));
 
-		for (int i = 0; i < program.length(); i++) {
-			String currentChar = String.valueOf(program.charAt(i));
-			try {
 				state = state.handleCharacter(currentChar, tokenList);
-			} catch (ScannerException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 
+			}
+			// handle end
+			state.handleCharacter("", tokenList);
+		} catch (ScannerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		tokenList.add(new Base(Terminals.SENTINEL));
 		return tokenList;
 	}
 
