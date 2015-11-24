2c5cb172c690998bca521624511c2d66e36f070b
jakobruder <jakob.ruder@ubs.com>
2015-11-24 20:03:32 +0100

    Changed Literals for boolean

--- test/src/ch/fhnw/cpib/scanner/Literal.java
+++ test/src/ch/fhnw/cpib/scanner/Literal.java
@@ -5,17 +5,27 @@ import ch.fhnw.cpib.scanner.symbols.Base;
 
 public class Literal extends Base {
 
-	private final int value;
-	
+	private final String value;
+
 	public Literal(int value) {
 		super(Terminals.LITERAL);
-		this.value = value;
+		this.value = String.valueOf(value);
 	}
-	
-	public int getValue() {
+
+	public Literal(boolean value) {
+		super(Terminals.LITERAL);
+		if (value) {
+			this.value = "BoolVal true";
+		} else {
+			this.value = "BoolVal false";
+		}
+	}
+
+	public String getValue() {
 		return value;
 	}
-	
+
 	public String toString() {
 		return "(" + super.toString() + "," + value + ")";
-	}}
+	}
+}
