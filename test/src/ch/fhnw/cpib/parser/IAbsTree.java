a5926690d91ec06b2c1f061ba6eeda39db98af59
jakobruder <jakob.ruder@ubs.com>
2016-01-04 19:29:47 +0100

    AbsTree

--- test/src/ch/fhnw/cpib/parser/IAbsTree.java
+++ test/src/ch/fhnw/cpib/parser/IAbsTree.java
@@ -1,18 +1,20 @@
 package ch.fhnw.cpib.parser;
 
+import java.util.ArrayList;
+
 import ch.fhnw.cpib.scanner.Ident;
 import ch.fhnw.cpib.scanner.Literal;
 
 public interface IAbsTree {
 
 	public interface IAbsExpr {
-		
+
 	}
-	
+
 	public interface IAbsCmd {
-		
+
 	}
-	
+
 	public class LiteralExpr implements IAbsExpr {
 		private Literal literal;
 
@@ -20,10 +22,9 @@ public interface IAbsTree {
 			super();
 			this.literal = literal;
 		}
-		
-		
+
 	}
-	
+
 	public class StoreExpr implements IAbsExpr {
 		private Ident ident;
 		private boolean isInitialization;
@@ -34,48 +35,104 @@ public interface IAbsTree {
 			this.isInitialization = isInitialization;
 		}
 	}
-	
+
 	public class FunCallExpr implements IAbsExpr {
-		
+
 	}
-	
+
 	public class MonadicExpr implements IAbsExpr {
-		
+
 	}
-	
+
 	public class DyadicExpr implements IAbsExpr {
-		
+
 	}
-	
+
 	public class SkipCmd implements IAbsCmd {
-		
+
 	}
-	
+
 	public class AssiCmd implements IAbsCmd {
-		
+		private IAbsExpr expr1;
+		private IAbsExpr expr2;
+
+		public AssiCmd(IAbsExpr expr1, IAbsExpr expr2) {
+			super();
+			this.expr1 = expr1;
+			this.expr2 = expr2;
+		}
+
 	}
-	
+
 	public class CpsCmd implements IAbsCmd {
-		
+		private ArrayList<IAbsCmd> cmdList;
+
+		public CpsCmd(ArrayList<IAbsCmd> cmdList) {
+			super();
+			this.cmdList = cmdList;
+		}
+
 	}
-	
+
 	public class CondCmd implements IAbsCmd {
-		
+		private IAbsExpr expr;
+		private IAbsCmd cmd1;
+		private IAbsCmd cmd2;
+
+		public CondCmd(IAbsExpr expr, IAbsCmd cmd1, IAbsCmd cmd2) {
+			super();
+			this.expr = expr;
+			this.cmd1 = cmd1;
+			this.cmd2 = cmd2;
+		}
+
 	}
-	
+
 	public class WhileCmd implements IAbsCmd {
-		
+		private IAbsExpr expr;
+		private IAbsCmd cmd;
+
+		public WhileCmd(IAbsExpr expr) {
+			super();
+			this.expr = expr;
+		}
+
 	}
-	
+
 	public class ProcCallCmd implements IAbsCmd {
-		
+		private Ident ident;
+		private Ident identRoutine;
+		private ArrayList<IAbsExpr> exprListRoutine;
+		private ArrayList<Ident> identList;
+
+		public ProcCallCmd(Ident ident, Ident identRoutine,
+				ArrayList<IAbsExpr> exprListRoutine, ArrayList<Ident> identList) {
+			super();
+			this.ident = ident;
+			this.identRoutine = identRoutine;
+			this.exprListRoutine = exprListRoutine;
+			this.identList = identList;
+		}
+
 	}
-	
+
 	public class InputCmd implements IAbsCmd {
-		
+		private IAbsExpr expr;
+
+		public InputCmd(IAbsExpr expr) {
+			super();
+			this.expr = expr;
+		}
+
 	}
-	
+
 	public class OutputCmd implements IAbsCmd {
-		
+		private IAbsExpr expr;
+
+		public OutputCmd(IAbsExpr expr) {
+			super();
+			this.expr = expr;
+		}
+
 	}
 }
