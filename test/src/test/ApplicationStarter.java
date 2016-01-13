f4363297ea6c295d2442486a49f2a64854828669
Jonathan Spielvogel <jonathan.spielvogel@students.fhnw.ch>
2016-01-13 14:08:54 +0100

    [Testprogramme] Testprogramme werden jetzt aus File gelesen, Tabs werden erkannt und entfernt -> Lesbarer Code als Eingabe

--- test/src/test/ApplicationStarter.java
+++ test/src/test/ApplicationStarter.java
@@ -1,5 +1,11 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import ch.fhnw.cpib.checker.ContextError;
 import ch.fhnw.cpib.parser.IAbsTree;
 import ch.fhnw.cpib.parser.Parser;
@@ -13,60 +19,49 @@ import ch.fhnw.cpib.virtualMachine.VirtualMachine;
 
 public class ApplicationStarter {
 
-	public static void main(String[] args) throws GrammarError, ContextError,
-			CodeTooSmallError, ExecutionError {
-		String test = "program isEligibleForMaster ()"
-				+ "global "
-				+ "credits: int64; "
-				+ "gradeTimesTen: int64; "
-				+ "age: int64; "
-				+ "recommendation: bool "
-				+ "do "
-				+ "debugin credits init; "
-				+ "debugin gradeTimesTen init; "
-				+ "debugin age init; "
-				+ "debugin recommendation init; "
-				+ "if credits > 150 &? gradeTimesTen > 50 &? (age < 35 |? recommendation) then "
-				+ "debugout true " + "else " + "debugout false endif "
-				+ "endprogram";
-		String test2 = "program isSorted () " + "global " + "a: int64; "
-				+ "b: int64; " + "c: int64 " + "do " + "debugin a init; "
-				+ "debugin b init; " + "debugin c init; "
-				+ "if ( a <= b <= c ) then " + "debugout true " + "else "
-				+ "if ( a >= b >= c ) then " + "debugout true " + "else "
-				+ "debugout false " + "endif " + "endif " + "endprogram";
-		String test3 = "program isSorted ()"
-				+ " global"
-				+ " a: int64;"
-				+ " b: int64;"
-				+ " c: int64;"
-				+ " proc sorted(in copy d:int64, in copy e:int64, in copy f:int64)"
-				+ " do"
-				+ " if ( d <= e <= f ) then"
-				+ " debugout true"
-				+ " else"
-				+ " if ( d >= e >= f ) then"
-				+ " debugout true"
-				+ " else"
-				+ " debugout false"
-				+ " endif"
-				+ " endif"
-				+ " endproc"
-				+ " do"
-				+ " debugin a init;"
-				+ " debugin b init;"
-				+ " debugin c init;"
-				+ " call sorted(a, b, c)"
-				+ " endprogram";
+	public static void main(String[] args)
+			throws GrammarError, ContextError, CodeTooSmallError, ExecutionError, IOException {
+
+		String program = readFromFile("testIML.txt");
 		
+		
+		
+//		String test = "program isEligibleForMaster ()" + "global " + "credits: int64; " + "gradeTimesTen: int64; "
+//				+ "age: int64; " + "recommendation: bool " + "do " + "debugin credits init; "
+//				+ "debugin gradeTimesTen init; " + "debugin age init; " + "debugin recommendation init; "
+//				+ "if credits > 150 &? gradeTimesTen > 50 &? (age < 35 |? recommendation) then " + "debugout true "
+//				+ "else " + "debugout false endif " + "endprogram";
+//		String test2 = "program isSorted () " + "global " + "a: int64; " + "b: int64; " + "c: int64 " + "do "
+//				+ "debugin a init; " + "debugin b init; " + "debugin c init; " + "if ( a <= b <= c ) then "
+//				+ "debugout true " + "else " + "if ( a >= b >= c ) then " + "debugout true " + "else "
+//				+ "debugout false " + "endif " + "endif " + "endprogram";
+//		String test3 = "program isSorted ()" + " global" + " a: int64;" + " b: int64;" + " c: int64;"
+//				+ " proc sorted(in copy d:int64, in copy e:int64, in copy f:int64)" + " do" + " if ( d <= e <= f ) then"
+//				+ " debugout true" + " else" + " if ( d >= e >= f ) then" + " debugout true" + " else"
+//				+ " debugout false" + " endif" + " endif" + " endproc" + " do" + " debugin a init;" + " debugin b init;"
+//				+ " debugin c init;" + " call sorted(a, b, c)" + " endprogram";
+
 		Scanner scanner = new Scanner();
-		IParser parser = new Parser(scanner.scan(test3.subSequence(0,
-				test3.length())));
+		IParser parser = new Parser(scanner.scan(program.subSequence(0, program.length())));
 		IAbsTree.Program tree = parser.parse().toAbs();
 		tree.check();
 		ICodeArray array = tree.generateCode();
 		VirtualMachine vm = new VirtualMachine(array, 500);
 		System.out.println("test");
+
+	}
+
+	public static String readFromFile(String url) throws IOException, FileNotFoundException {
+		BufferedReader br = new BufferedReader(new FileReader(new File(url)));
+		String program = "";
+		String s = "";
+		while ((s = br.readLine()) != null) {
+			program += s += " ";
+		}
+		
+		program = program.replaceAll("\t", " ");
+		br.close();
+		return program;
 
 	}
 
